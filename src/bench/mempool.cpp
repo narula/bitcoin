a1d32df4f4f094349cac7764fba0d3b5bc8a537a
Neha Narula <narula@gmail.com>
2016-09-16 15:05:53 -0400

    Add a bit more to a mempool benchmark

--- src/bench/mempool.cpp
+++ src/bench/mempool.cpp
@@ -5,17 +5,60 @@
 #include "bench.h"
 
 #include "main.h"
-#include "test/test_bitcoin.h"
+#include "bench/test_bitcoin.h"
+#include "txmempool.h"
 
 #include <vector>
 #include <string>
 
 
-static void Basic(benchmark::State& state)
+static void Mempool(benchmark::State& state)
 {
+    TestMemPoolEntryHelper entry;
+    // Parent transaction with three children,
+    // and three grand-children:
+    CMutableTransaction txParent;
+    txParent.vin.resize(1);
+    txParent.vin[0].scriptSig = CScript() << OP_11;
+    txParent.vout.resize(3);
+    for (int i = 0; i < 3; i++)
+    {
+        txParent.vout[i].scriptPubKey = CScript() << OP_11 << OP_EQUAL;
+        txParent.vout[i].nValue = 33000LL;
+    }
+    CMutableTransaction txChild[3];
+    for (int i = 0; i < 3; i++)
+    {
+        txChild[i].vin.resize(1);
+        txChild[i].vin[0].scriptSig = CScript() << OP_11;
+        txChild[i].vin[0].prevout.hash = txParent.GetHash();
+        txChild[i].vin[0].prevout.n = i;
+        txChild[i].vout.resize(1);
+        txChild[i].vout[0].scriptPubKey = CScript() << OP_11 << OP_EQUAL;
+        txChild[i].vout[0].nValue = 11000LL;
+    }
+    CMutableTransaction txGrandChild[3];
+    for (int i = 0; i < 3; i++)
+    {
+        txGrandChild[i].vin.resize(1);
+        txGrandChild[i].vin[0].scriptSig = CScript() << OP_11;
+        txGrandChild[i].vin[0].prevout.hash = txChild[i].GetHash();
+        txGrandChild[i].vin[0].prevout.n = 0;
+        txGrandChild[i].vout.resize(1);
+        txGrandChild[i].vout[0].scriptPubKey = CScript() << OP_11 << OP_EQUAL;
+        txGrandChild[i].vout[0].nValue = 11000LL;
+    }
+
+
     CTxMemPool testPool(CFeeRate(0));
+    std::list<CTransaction> removed;
+
+
     while (state.KeepRunning()) {
+        testPool.addUnchecked(txParent.GetHash(), entry.FromTx(txParent));
+        testPool.removeRecursive(txParent, removed);
+        removed.clear();
     }
 }
 
-BENCHMARK(Basic);
+BENCHMARK(Mempool);
---
+++ src/bench/test_bitcoin.cpp
@@ -0,0 +1,53 @@
+// Copyright (c) 2011-2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "test_bitcoin.h"
+
+#include "chainparams.h"
+#include "consensus/consensus.h"
+#include "consensus/validation.h"
+#include "key.h"
+#include "main.h"
+#include "miner.h"
+#include "pubkey.h"
+#include "random.h"
+#include "txdb.h"
+#include "txmempool.h"
+#include "ui_interface.h"
+#include "rpc/server.h"
+#include "rpc/register.h"
+
+#include <boost/filesystem.hpp>
+#include <boost/thread.hpp>
+
+
+
+CTxMemPoolEntry TestMemPoolEntryHelper::FromTx(CMutableTransaction &tx, CTxMemPool *pool) {
+    CTransaction txn(tx);
+    return FromTx(txn, pool);
+}
+
+CTxMemPoolEntry TestMemPoolEntryHelper::FromTx(CTransaction &txn, CTxMemPool *pool) {
+    bool hasNoDependencies = pool ? pool->HasNoInputsOf(txn) : hadNoDependencies;
+    // Hack to assume either its completely dependent on other mempool txs or not at all
+    CAmount inChainValue = hasNoDependencies ? txn.GetValueOut() : 0;
+
+    return CTxMemPoolEntry(txn, nFee, nTime, dPriority, nHeight,
+                           hasNoDependencies, inChainValue, spendsCoinbase, sigOpCost, lp);
+}
+
+void Shutdown(void* parg)
+{
+  exit(0);
+}
+
+void StartShutdown()
+{
+  exit(0);
+}
+
+bool ShutdownRequested()
+{
+  return false;
+}
